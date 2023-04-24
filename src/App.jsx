d9a9eb21b12ebc896b0320ad79ce11e43ddaeb68
Your Name <you@example.com>
2023-04-24 15:24:02 +0530

    please work

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 
-import './App.css'
-import Aboutus from './Aboutus'
 function App() {
-
 
   return (
     <>
-      <div >
-        <h1>hello </h1>
-        </div>
-        <Aboutus />
+        <h1>hello</h1>
       </>
   )
 }
