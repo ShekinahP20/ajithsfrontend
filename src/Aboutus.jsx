4bb3f56357641f08ad7793fd302617463d231a25
Your Name <you@example.com>
2023-04-24 13:14:25 +0530

    first

---
+++ src/Aboutus.jsx
@@ -0,0 +1,9 @@
+function Aboutus(){
+return(
+    <>
+    <h1>hello this is about us</h1>
+    </>
+)
+}
+
+export default Aboutus
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Aboutus from './Aboutus'
+function App() {
+
+
+  return (
+    <>
+      <div >
+        <h1>app page</h1>
+        </div>
+        <Aboutus />
+      </>
+  )
+}
+
+export default App
